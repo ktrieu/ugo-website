87ba008ebc16e9e0a36af229b8479c050d7084a6
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-05-01 02:07:46 -0400

    Clear all the default code out of the project
    And convert the index page to Typescript.

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <>
+      <h1>UNDER CONSTRUCTION</h1>
+      <p>Estimated Completion: UGO Day 2021</p>
+    </>
+  );
+};
+
+export default IndexPage;
