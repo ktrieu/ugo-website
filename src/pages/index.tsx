80d750f75faf198ad11bf79a77ffbbbfb5bcffce
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-05-04 01:11:06 -0400

    Add some hero buttons to the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
+import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
+import { IconType } from "react-icons";
+import { FaBoxes, FaClock, FaInfo } from "react-icons/fa";
 import Layout from "../components/Layout";
+
+interface HeroButtonProps {
+  to: string;
+  name: string;
+  icon: IconType;
+}
+
+const HeroButton: React.FC<HeroButtonProps> = (props) => {
+  const ButtonIcon = props.icon;
+  return (
+    <Link
+      className="border-2 border-primary text-primary p-6 flex flex-col items-center"
+      to={props.to}
+    >
+      <ButtonIcon size="50" className="fill-current" />
+      <p className="mt-3 text-2xl">{props.name}</p>
+    </Link>
+  );
+};
 
 const IndexPage = () => {
   return (
@@ -15,10 +37,13 @@ const IndexPage = () => {
           <h1 className="text-3xl sm:text-8xl">
             A company you have to believe in.
           </h1>
-          <p className="mt-6 sm:text-4xl">
-            Building today's technology, today.
-          </p>
+          <p className="mt-6 sm:text-4xl">Building today's products, today.</p>
         </div>
+      </div>
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-3">
+        <HeroButton to="/about" icon={FaInfo} name="About Us" />
+        <HeroButton to="/history" icon={FaClock} name="Our History" />
+        <HeroButton to="/products" icon={FaBoxes} name="Our Products" />
       </div>
     </Layout>
   );
