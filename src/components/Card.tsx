f54989949e69bd95d0ad2e88a82fa0565e5df675
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-05-06 02:56:24 -0400

    Remove props from Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,6 @@
-import { IGatsbyImageData, GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
 
-interface HQCardProps {
-  name: string;
-  location: string;
-  html: string;
-  photo: IGatsbyImageData;
-}
-
-const HQCard: React.FC<HQCardProps> = (props) => {
+const HQCard: React.FC = (props) => {
   return (
     <div className="rounded p-3 border-2 border-primary mt-3 shadow-lg">
       {props.children}
